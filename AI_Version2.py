82deccd28d11409188df534da0e1c5189e7915ee
EdisonNg14 <nge-pm22@student.tarc.edu.my>
2024-09-14 01:27:22 +0800

    Update AI_Version2.py

--- AI_Version2.py
+++ AI_Version2.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from collections import Counter
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics.pairwise import cosine_similarity
