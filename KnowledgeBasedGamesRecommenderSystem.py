9d08f96a0a0b588e856ef6717fd8744bbf3482d6
EdisonNg14 <nge-pm22@student.tarc.edu.my>
2024-09-14 16:28:23 +0800

    Update KnowledgeBasedGamesRecommenderSystem.py

--- KnowledgeBasedGamesRecommenderSystem.py
+++ KnowledgeBasedGamesRecommenderSystem.py
@@ -14,7 +14,7 @@ def main():
     st.markdown("""
     <style>
     .main {
-        background-color: #f0f0f5;
+        background-color: #000000;
     }
     .sidebar .sidebar-content {
         background-color: #f0f0f5;
