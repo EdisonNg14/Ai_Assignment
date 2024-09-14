df3ab10f28ecc67f68dfa749a88d90e3db2c2e5d
EdisonNg14 <nge-pm22@student.tarc.edu.my>
2024-09-15 00:14:47 +0800

    Update Combinee.py

--- Combinee.py
+++ Combinee.py
@@ -78,8 +78,8 @@ page = st.session_state.page
 
 # Home Page
 if page == "Home":
-    css_code="""<style> background-image: url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dgame%2Bbackground&psig=AOvVaw3_XR93klPh6Vp6Ad2KZE4Z&ust=1726416341845000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJjf-5vowogDFQAAAAAdAAAAABAE");</style>"""
-    st.markdown(css_code, unsafe_allow_html=True)
+    css_code="""<style> background-image: url("360_F_88981880_YjJManMJ6hJmKr5CZteFJAkEzXIh8mxW.jpg");</style>"""
+    st.markdown1(css_code, unsafe_allow_html=True)
     st.title("🎮 Welcome to the Game Recommendation System")
     st.markdown("""
     Welcome to the *Game Recommendation System*! This app provides various ways to find your next favorite video game.
