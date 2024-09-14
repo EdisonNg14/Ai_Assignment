b43149573b6ccda1a788ce6184c77344607a2f57
EdisonNg14 <nge-pm22@student.tarc.edu.my>
2024-09-14 17:52:49 +0800

    Update Combinee.py

--- Combinee.py
+++ Combinee.py
@@ -76,7 +76,10 @@ def recommend_games(df, preferences):
 
 # Navigation Sidebar
 st.sidebar.title("Navigation")
-page = st.sidebar.selectbox("Go to", ["Home", "Content-Based Recommendations", "Top 10 Recommendation based on User Preference", "Game Correlation Finder", "About"])
+page = st.sidebar.radio(
+    "Go to",
+    ["Home", "Content-Based Recommendations", "Top 10 Recommendation based on User Preference", "Game Correlation Finder", "About"]
+)
 
 # Home Page
 if page == "Home":
