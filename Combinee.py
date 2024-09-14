5a0f8aa27103e4860f4eabf1a42c559da337e286
EdisonNg14 <nge-pm22@student.tarc.edu.my>
2024-09-14 17:16:58 +0800

    Update Combinee.py

--- Combinee.py
+++ Combinee.py
@@ -15,7 +15,7 @@ st.set_page_config(
 st.markdown("""
     <style>
     .stApp {
-        background-color: #f0f0f5;  /* Light background color */
+        background-color: Black;  /* Light background color */
     }
     .stButton>button {
         background-color: #4CAF50;
